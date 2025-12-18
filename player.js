2337f51043e3f83059c5d4fae249281d4d2ea92e
redes11111 <redes11111@gmail.com>
2025-12-18 23:28:02 +0200

    Update player.js

--- player.js
+++ player.js
@@ -4,15 +4,15 @@ const now = document.getElementById("now");
 const stations = {
   love: {
     name: "Love Radio",
-    url: "https://stream1.loveradio.ru/love128.mp3"
+    url: "https://stream.loveradio.ru/12_love_64.mp3"
   },
   new: {
     name: "Новое Радио",
     url: "https://stream.newradio.ru/moscow.novoe.aacp"
   },
-  gaming: {
-    name: "Gaming Radio",
-    url: "https://pub0101.101.ru/stream/pro/mp3/128/741960"
+  power: {
+    name: "Сила любви",
+    url: "https://stream.powerapp.com.tr/powerlove/mp3"
   },
   night: {
     name: "Night Vibe",
