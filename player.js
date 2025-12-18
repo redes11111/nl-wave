45fd336606170f6f798eb316617722467db1dd8c
redes11111 <redes11111@gmail.com>
2025-12-19 00:13:37 +0200

    Update player.js

--- player.js
+++ player.js
@@ -4,23 +4,23 @@ const now = document.getElementById("now");
 const stations = {
   love: {
     name: "Love Radio",
-    url: "https://icecast.vgtrk.cdnvideo.ru/loveradio_m.mp3"
+    url: "https://88.210.36.164/loveradio_m.mp3"
   },
   new: {
     name: "Новое Радио",
     url: "https://stream.newradio.ru/moscow.novoe.aacp"
   },
-  power: {
-    name: "Сила любви",
-    url: "https://listen.powerapp.com.tr/powerlove/abr/powerlove/128/chunks.m3u8"
+  tht: {
+    name: "THT Music Radio",
+    url: "https://stream1.radiopotok.ru/tn.mp3"
   },
   night: {
     name: "Night Vibe",
     url: "https://radio.plaza.one/mp3"
   },
-  hard: {
-    name: "Hard Mode",
-    url: "https://stream.rcast.net/13988"
+  rekord: {
+    name: "Радио Рекорд",
+    url: "https://23.111.104.140/rr96.aacp"
   }
 };
 
