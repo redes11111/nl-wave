3f48423b2e5ccb3ac7eaebaa945ae12e657778cf
redes11111 <redes11111@gmail.com>
2025-12-25 18:04:00 +0200

    Create tracks.js

---
+++ tracks.js
@@ -0,0 +1,11 @@
+const PLAYLISTS = {
+  local: [
+    "music/local/HOLLYFLAME - Красками.mp3",
+    "music/local/Post_Malone_Swae_Lee_-_Sunflower_59804834.mp3"
+  ],
+
+  love: [
+    "AudioCutter_ke(2).mp3",
+    "AudioCutter_ke(3).mp3"
+  ]
+};
